1560fc391d8038a34b56816e568ce91e623f47e8
unknown <clt7680seal@gmail.com>
2026-03-10 11:17:55 +0000

    Added new route

--- Express.js
+++ Express.js
@@ -38,20 +38,44 @@ import express from "express";
 
 const app = express();
 
+// JSON user object
+const user = {
+    name: "John Doe",
+    email: "JohnDoe"
+};
+
+// Json list of products
+const products = [
+    { id: 1, name: "Product 1", price: 10 },
+    { id: 2, name: "Product 2", price: 20 },
+    { id: 3, name: "Product 3", price: 30 }
+];
+
 // Home route
 app.get("/", (req, res) => {
-    res.send("<h1>Welcome to the HomePage</h1>");
+    res.send("<h1>Welcome to Express server</h1>");
 });
 
-// Login route
-app.get("/login", (req, res) => {
-    res.send("<h1>Welcome to the Login Page</h1>");
+// about route
+app.get("/about", (req, res) => {
+    res.send("<h1>About this application</h1>");
 });
 
-// Logout route
-app.get("/logout", (req, res) => {
-    res.send("<h1>You successfully LoggedOut</h1>");
+// contact route
+app.get("/contact", (req, res) => {
+    res.send("<h3>Contact page</h3>")
 });
+
+
+// produce route
+app.get("/products", (req, res) => {
+    res.json(products);   // sends JSON response
+});
+
+// user route
+app.get("/user", (req, res) => {
+    res.json(user);   // sends JSON response
+})
 
 // 404 route
 app.use((req, res) => {
